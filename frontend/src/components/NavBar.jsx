e31fb12ecb2b0fd48323c5721acc8e3dbeed0d57
Nathan <nathansynniang@gmail.com>
2026-04-17 03:29:40 +0530

    app complete

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -6,7 +6,7 @@ const NavBar = () => {
     <header className="bg-base-300 border-b border-base-content/10">
     <div className="mx-auto max-w-6xl p-4">
       <div className="flex items-center justify-between">
-        <h1 className="text-3xl font-bold text-primary font-mono tracking-tight">ThinkBoard</h1>
+        <h1 className="text-3xl font-bold text-primary font-mono tracking-tight">Notes Dashboard</h1>
         <div className="flex items-center gap-4">
           <Link to="/create" className="btn btn-primary">
             <PlusIcon className="size-5" />
